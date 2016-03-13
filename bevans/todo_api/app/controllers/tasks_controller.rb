948e496e9b9554aaa84ef5af303fc52a2e0624c9
Ben Evans <bevans@zendesk.com>
2016-03-13 15:56:20 -0500

    added byebug, make controller create method work

--- bevans/todo_api/app/controllers/tasks_controller.rb
+++ bevans/todo_api/app/controllers/tasks_controller.rb
@@ -1,5 +1,16 @@
 class TasksController < ApplicationController
+	skip_before_filter :verify_authenticity_token
 	def index
-		render json: @tasks = Task.all
+		render json: Task.all
+	end
+	def create
+		@task = Task.new(json_data['task'])
+		@task.save
+		render json: @task
+	end
+	private
+	def json_data
+		request.body.rewind
+		@json_data ||= JSON.parse(request.body.read)
 	end
 end
