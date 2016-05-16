2572af3e373f002c48f8b11f69c8fea49eb569e7
Ben Evans <bevans@zendesk.com>
2016-05-15 21:17:12 -0500

    working CRUD

--- bevans/todo_ui/app/routes/tasks/index.js
+++ bevans/todo_ui/app/routes/tasks/index.js
@@ -5,5 +5,19 @@ export default Ember.Route.extend({
 
   model() {
     return this.store.findAll('task');
+  },
+
+  actions: {
+    saveTask(newTask) {
+      newTask.save();
+    },
+
+    deleteTask(task) {
+      let confirmation = confirm('Are you sure?');
+
+      if (confirmation) {
+        task.destroyRecord();
+      }
+    }
   }
 });
